459b027554d3812c966f563e2f431bbbed4d2a97
jingxian0320 <baojingxian0320@gmail.com>
2018-01-10 00:41:12 +0800

    update

--- script.py
+++ script.py
@@ -34,3 +34,80 @@ print(train.shape)
 print(test.shape)
 
 train.head(3)
+
+#PROCESS CATEGORICAL DATA
+print("Handling categorical variables...")
+le = LabelEncoder()
+
+le.fit(np.hstack([train.category_name, test.category_name]))
+train.category_name = le.transform(train.category_name)
+test.category_name = le.transform(test.category_name)
+
+le.fit(np.hstack([train.brand_name, test.brand_name]))
+train.brand_name = le.transform(train.brand_name)
+test.brand_name = le.transform(test.brand_name)
+del le
+
+train.head(3)
+
+
+#PROCESS TEXT: RAW
+print("Text to seq process...")
+from keras.preprocessing.text import Tokenizer
+raw_text = np.hstack([train.item_description.str.lower(), train.name.str.lower()])
+
+print("   Fitting tokenizer...")
+tok_raw = Tokenizer()
+tok_raw.fit_on_texts(raw_text)
+print("   Transforming text to seq...")
+
+train["seq_item_description"] = tok_raw.texts_to_sequences(train.item_description.str.lower())
+test["seq_item_description"] = tok_raw.texts_to_sequences(test.item_description.str.lower())
+train["seq_name"] = tok_raw.texts_to_sequences(train.name.str.lower())
+test["seq_name"] = tok_raw.texts_to_sequences(test.name.str.lower())
+train.head(3)
+
+#SEQUENCES VARIABLES ANALYSIS
+max_name_seq = np.max([np.max(train.seq_name.apply(lambda x: len(x))), np.max(test.seq_name.apply(lambda x: len(x)))])
+max_seq_item_description = np.max([np.max(train.seq_item_description.apply(lambda x: len(x)))
+                                   , np.max(test.seq_item_description.apply(lambda x: len(x)))])
+print("max name seq "+str(max_name_seq))
+print("max item desc seq "+str(max_seq_item_description))
+
+#PROCESS TEXT: RAW
+print("Text to seq process...")
+from keras.preprocessing.text import Tokenizer
+raw_text = np.hstack([train.item_description.str.lower(), train.name.str.lower()])
+
+print("   Fitting tokenizer...")
+tok_raw = Tokenizer()
+tok_raw.fit_on_texts(raw_text)
+print("   Transforming text to seq...")
+
+train["seq_item_description"] = tok_raw.texts_to_sequences(train.item_description.str.lower())
+test["seq_item_description"] = tok_raw.texts_to_sequences(test.item_description.str.lower())
+train["seq_name"] = tok_raw.texts_to_sequences(train.name.str.lower())
+test["seq_name"] = tok_raw.texts_to_sequences(test.name.str.lower())
+train.head(3)
+
+#SEQUENCES VARIABLES ANALYSIS
+max_name_seq = np.max([np.max(train.seq_name.apply(lambda x: len(x))), np.max(test.seq_name.apply(lambda x: len(x)))])
+max_seq_item_description = np.max([np.max(train.seq_item_description.apply(lambda x: len(x)))
+                                   , np.max(test.seq_item_description.apply(lambda x: len(x)))])
+print("max name seq "+str(max_name_seq))
+print("max item desc seq "+str(max_seq_item_description))
+
+train.seq_name.apply(lambda x: len(x)).hist()
+train.seq_item_description.apply(lambda x: len(x)).hist()
+
+#EMBEDDINGS MAX VALUE
+#Base on the histograms, we select the next lengths
+MAX_NAME_SEQ = 10
+MAX_ITEM_DESC_SEQ = 75
+MAX_TEXT = np.max([np.max(train.seq_name.max())
+                   , np.max(test.seq_name.max())
+                  , np.max(train.seq_item_description.max())
+                  , np.max(test.seq_item_description.max())])+2
+MAX_CATEGORY = np.max([train.category_name.max(), test.category_name.max()])+1
+MAX_BRAND = np.max([train.brand_name.max(), test.brand_name.max()])+1
+MAX_CONDITION = np.max([train.item_condition_id.max(), test.item_condition_id.max()])+1
